b23c61d251fb8071d2eb2c3554139b3241192c5f
Daniel Bluhm <dbluhm@pm.me>
2023-10-20 18:57:40 -0400

    refactor: compress test cases for valid
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- tests/test_valid.py
+++ tests/test_valid.py
@@ -1,6 +1,10 @@
+from itertools import product
 import json
+
 import pytest
+
 from did_peer_4 import validate_input_document
+
 from . import EXAMPLES
 
 
@@ -18,41 +22,27 @@ def test_validate_examples(example):
         {},
         {"id": "did:peer:4:123456789abcdefghi"},
         {"alsoKnownAs": "not a list"},
-        {"verificationMethod": "not a list"},
-        {"authentication": "not a list"},
-        {"assertionMethod": "not a list"},
-        {"keyAgreement": "not a list"},
-        {"capabilityDelegation": "not a list"},
-        {"capabilityInvocation": "not a list"},
-        {"service": "not a list"},
-        {"verificationMethod": [{}]},
-        {"authentication": [{}]},
-        {"assertionMethod": [{}]},
-        {"keyAgreement": [{}]},
-        {"capabilityDelegation": [{}]},
-        {"capabilityInvocation": [{}]},
-        {"service": [{}]},
-        {"verificationMethod": [{"id": "did:peer:4:123456789abcdefghi#key-1"}]},
-        {"authentication": [{"id": "did:peer:4:123456789abcdefghi#key-1"}]},
-        {"assertionMethod": [{"id": "did:peer:4:123456789abcdefghi#key-1"}]},
-        {"keyAgreement": [{"id": "did:peer:4:123456789abcdefghi#key-1"}]},
-        {"capabilityDelegation": [{"id": "did:peer:4:123456789abcdefghi#key-1"}]},
-        {"capabilityInvocation": [{"id": "did:peer:4:123456789abcdefghi#key-1"}]},
-        {"service": [{"id": "did:peer:4:123456789abcdefghi#key-1"}]},
-        {"verificationMethod": [{"id": "#key-1"}]},
-        {"authentication": [{"id": "#key-1"}]},
-        {"assertionMethod": [{"id": "#key-1"}]},
-        {"keyAgreement": [{"id": "#key-1"}]},
-        {"capabilityDelegation": [{"id": "#key-1"}]},
-        {"capabilityInvocation": [{"id": "#key-1"}]},
-        {"service": [{"id": "#key-1"}]},
-        {"verificationMethod": [{"id": 0}]},
-        {"authentication": [{"id": 0}]},
-        {"assertionMethod": [{"id": 0}]},
-        {"keyAgreement": [{"id": 0}]},
-        {"capabilityDelegation": [{"id": 0}]},
-        {"capabilityInvocation": [{"id": 0}]},
-        {"service": [{"id": 0}]},
+        *[
+            {k: v}
+            for k, v in product(
+                [
+                    "verificationMethod",
+                    "authentication",
+                    "assertionMethod",
+                    "keyAgreement",
+                    "capabilityDelegation",
+                    "capabilityInvocation",
+                    "service",
+                ],
+                [
+                    "not a list",
+                    [{}],
+                    [{"id": "did:peer:4:123456789abcdefghi#key-1"}],
+                    [{"id": "#key-1"}],
+                    [{"id": 0}],
+                ],
+            )
+        ],
     ],
 )
 def test_invalid(invalid):
